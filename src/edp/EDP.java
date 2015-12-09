17f7848324056a087ea3960d506bd4ef6a12a2e7
Alvaro <alvaroberrocal12@hotmail.com>
2015-12-09 22:58:12 +0100

    ..

--- src/edp/EDP.java
+++ src/edp/EDP.java
@@ -7,6 +7,11 @@ package edp;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.graphicGraph.GraphicGraph;
+import org.graphstream.ui.view.Viewer;
 
 /**
  *
@@ -17,10 +22,44 @@ public class EDP {
     /**
      * @param args the command line arguments
      */
+    
+    public static void printGraph(edp.Graph g, Solution s){
+        int n = g.getNodes();
+        org.graphstream.graph.Graph gr = new SingleGraph("G1"); 
+        for (int i=0; i<n;i++){
+           gr.addNode(""+i);
+        }
+        int [][] m = g.getAdjacent();
+        for (int i=0; i<n; i++){
+            for (int j =0; j<n; j++){
+                if (m[i][j]== 1){
+                    gr.addEdge(i+"-"+j, ""+i,""+j);
+                    m[j][i]= 0;
+                }
+                    
+            }
+        }
+        
+        for (Node node : gr) {
+			node.addAttribute("ui.label", node.getId());
+			node.addAttribute("ui.style", "size: 30px; "
+										+ "text-alignment: center;"
+										+ "shape: box;"
+										+ "fill-color: green;");
+        }
+        gr.addAttribute("ui.antialias", true);
+        gr.display();
+    }
+    
+    
+    
     public static void main(String[] args) {
-        Instance i = new Instance ("instancias/AS-BA.R-Wax.v100e217.bb","instancias/AS-BA.R-Wax.v100e217.rpairs.40.20" );
+        
+        long start =System.currentTimeMillis();
+        Instance i = new Instance ("instancias/AS-BA.R-Wax.v100e217.bb","instancias/AS-BA.R-Wax.v100e217.rpairs.10.1" );
         Solution s= new Solution();
 
+        
         ArrayList<Integer> del; 
         for (int j=0; j< i.getNodeMatrix().size();j++  ){
             del=Dijkstra.Dijkstra(i.getNodeMatrix().get(j) [0],i.getNodeMatrix().get(j) [1], i.getG().getAdjacent(), i, s);
@@ -28,7 +67,9 @@ public class EDP {
             i.getG().setAdjacent(Dijkstra.deleteEdges(i.getG().getAdjacent(), del));
         }
         System.out.println(s.toString());
-        
+        long end =System.currentTimeMillis();
+        long time = end - start;
+        System.out.println("Tiempo empleado: "+time+" ms");
         System.out.println("¿Desea ver las rutas?s/n");
         Scanner scan = new Scanner (System.in);
         String op=scan.nextLine();
--- src/edp/Instance.java
+++ src/edp/Instance.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 
+
 /**
  *
  * @author Alvaro Berrocal Martin - URJC
